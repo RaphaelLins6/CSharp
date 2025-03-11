9fb4374b057abf4d51decf08c79519d8d88ff7f8
Raphael Lins <rapharell06@gmail.com>
2025-03-11 14:56:54 -0300

    Atualização

--- ClassesEMetodos/Membros.cs
+++ ClassesEMetodos/Membros.cs
@@ -10,17 +10,9 @@ namespace CursoCSharp.ClassesEMetodos
     {
         public static void Executar() {
             Pessoa sicrano = new Pessoa();
-            sicrano.Nome = "Renato";
-            sicrano.Idade = 21;
-            //Console.WriteLine($"{sicrano.Nome} tem {sicrano.Idade} anos.");
-            sicrano.ApresentarNoConsole();
-            sicrano.Zerar();
-            sicrano.ApresentarNoConsole();
-            var fulano = new Pessoa();
-            fulano.Nome = "Beto";
-            fulano.Idade = 21;
-            var apresentacaoDoFulano = fulano.Apresentar();
-            Console.WriteLine(apresentacaoDoFulano);
-        }
+            sicrano.Nome = "Raphael";
+            sicrano.Idade = 26;
+            Console.WriteLine($"{sicrano.Nome} tem {sicrano.Idade} anos.");
+        } 
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
 using CursoCSharp.EstruturasDeControle;
+using CursoCSharp.ClassesEMetodos;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+
+                // Fundamentos
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Váriaveis e Constatntes - Fundamentos", VariaveisEConstantes.Executar},
@@ -22,6 +25,8 @@ namespace CursoCSharp {
                 {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
                 {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
                 {"Operadores Ternários - Fundamentos", OperadoresTernarios.Executar},
+
+                // Estruturas de Controle
                 {"Estrutura IF - Estruturas de controle", Estruturaif.Executar},
                 {"Estrutura IF-ELSE - Estruturas de controle", Estruturaifelse.Executar},
                 {"Estrutura IF-ELSEIF - Estruturas de controle", Estruturaifelseif.Executar},
@@ -33,6 +38,31 @@ namespace CursoCSharp {
                 {"Estrutura Break - Estruturas de controle", Estruturabreak.Executar},
                 {"Estrutura Continue - Estruturas de controle", Estruturacontinue.Executar},
 
+                // Classes e métodos
+                {"Membros - Classes e métodos", Membros.Executar},
+                //{"Construtores - Classes e métodos", Construtores.Executar},
+                //{"Métodos com Retorno - Classes e métodos", MetodosComRetorno.Executar},
+                //{"Métodos Estáticos - Classes e métodos", MetodosEstaticos.Executar},
+                //{"Atributos Estáticos - Classes e métodos", AtributosEstaticos.Executar},
+                //{"Desafio Atributo - Classes e métodos", DesafioAtributo.Executar},
+                //{"Params - Classes e métodos", Params.Executar},
+                //{"Parâmetros Nomeados - Classes e métodos", ParametrosNomeados.Executar},
+                //{"Get Set - Classes e métodos", GetSet.Executar},
+                //{"Propriedades - Classes e métodos", Propriedades.Executar},
+                //{"Atributos Readonly - Classes e métodos", AtributosReadonly.Executar},
+                //{"Enum - Classes e métodos", Enumeracoes.Executar},
+                //{"Struct - Classes e métodos", Struct.Executar},
+                //{"Struct Vs Classe - Classes e métodos", StructVsClasse.Executar},
+                //{"Valor Vs Referência - Classes e métodos", ValorVsReferencia.Executar},
+                //{"Parâmetros por Referência - Classes e métodos", ParametrosPorReferencia.Executar},
+                //{"Parâmetros com Valor Padrão - Classes e métodos", ParametrosComValorPadrao.Executar},
+                //{"Delegates - Classes e métodos", Delegates.Executar},
+                //{"Lambda - Classes e métodos", Lambdas.Executar},
+                //{"Linq #01 - Classes e métodos", Linq1.Executar},
+                //{"Linq #02 - Classes e métodos", Linq2.Executar},
+                //{"Nullables - Classes e métodos", Nullables.Executar},
+                //{"Dynamics - Classes e métodos", Dynamics.Executar},
+                //{"Generics - Classes e métodos", Genericos.Executar},
             });
 
             central.SelecionarEExecutar();
