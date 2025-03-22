0e2a6093376e982b9960c1e7112d23cda125617f
Raphael Lins <rapharell06@gmail.com>
2025-03-21 23:44:04 -0300

    Update

--- ClassesEMetodos/MetodosComRetorno.cs
+++ ClassesEMetodos/MetodosComRetorno.cs
@@ -22,6 +22,29 @@ namespace CursoCSharp.ClassesEMetodos {
         }
     }
 
+    class CalculadoraCadeia() {
+        int memoria;
+        public CalculadoraCadeia Somar(int a) {
+            memoria += a;
+            return this;
+        }
+        public CalculadoraCadeia Multiplicar(int a) {
+            memoria *= a;
+            return this;
+        }
+        public CalculadoraCadeia Limpar() {
+            memoria = 0;
+            return this;
+        }
+        public CalculadoraCadeia Imprimir() {
+            Console.WriteLine(memoria);
+            return this;
+        }
+        public int Resultado() {
+            return memoria;
+        }   
+    }
+
     // Cria uma classe que chama os métodos da classe CalculadoraComum
     class MetodosComRetorno{
 
@@ -37,7 +60,13 @@ namespace CursoCSharp.ClassesEMetodos {
             Console.WriteLine(calculadoraComum.Multiplicar(30, 6));
             Console.Write("Resultado da divisão é: ");
             Console.WriteLine(calculadoraComum.Dividir(70, 7));
-            
+
+            var calculadoraCadeia = new CalculadoraCadeia();
+
+            calculadoraCadeia.Somar(3).Multiplicar(3).Imprimir().Limpar().Imprimir();
+            var resultado = calculadoraCadeia.Somar(3).Multiplicar(2).Resultado();
+            Console.WriteLine(resultado);
+
             Console.WriteLine("Pressione Enter para continuar...");
             Console.ReadLine();
         }
