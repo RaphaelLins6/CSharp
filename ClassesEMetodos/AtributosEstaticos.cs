bab42d57f2c6ac12078150265c2aaa7dee7ad393
Raphael Lins <rapharell06@gmail.com>
2025-03-22 23:32:43 -0300

    Update

---
+++ ClassesEMetodos/AtributosEstaticos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos {
+
+    public class Produto {
+
+        public string Nome;
+        public double Preco;
+        public double Desconto = 0.1;
+
+        public Produto(string nome, double preco, double desconto) {
+            Nome = nome;
+            Preco = preco;
+            Desconto = desconto;
+        }
+
+        public Produto() {
+            
+        }
+
+        public double CalcularDesc() {
+            return Preco - Preco * Desconto;
+        }
+    }
+
+    class AtributosEstaticos {
+
+        public static void Executar() {
+
+            var produto1 = new Produto("Caneta", 3.2, 0.1);
+            var produto2 = new Produto() {
+                Nome = "Borracha",
+                Preco = 5.3,
+                Desconto =  0.1
+            };
+
+            Console.WriteLine("Preço com desconto: {0}", produto1.CalcularDesc());
+            Console.WriteLine("Preço com desconto: {0}", produto2.CalcularDesc());
+
+            Console.WriteLine("Pressione Enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassesEMetodos/MetodosEstaticos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos {
+
+    class CalculadoraEstatica {
+        // Método de Classe ou Método Estático
+        public static int Multiplicar(int a, int b) {
+            return a * b;
+        }
+        // Método de Instância
+        public int Somar(int a, int b) {
+            return a + b;
+        }
+    }
+
+    class MetodosEstaticos {
+
+        public static void Executar() {
+            // Método de Classe ou Método Estático
+            var resultado = CalculadoraEstatica.Multiplicar(2, 2);
+            Console.WriteLine("Resultado: {0}", resultado);
+            // Método de Instância
+            CalculadoraEstatica calc = new CalculadoraEstatica();
+            Console.Write("Resultado: ");
+            Console.WriteLine(calc.Somar(2, 2));
+
+            Console.WriteLine("Pressione Enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
