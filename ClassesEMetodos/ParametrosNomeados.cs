b14e027b82d537db67f0755046a3e58e0476e523
Raphael Lins <rapharell06@gmail.com>
2025-03-24 22:48:28 -0300

    Update

--- ClassesEMetodos/ParametrosNomeados.cs
+++ ClassesEMetodos/ParametrosNomeados.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace CursoCSharp.ClassesEMetodos {
     class ParametrosNomeados {
 
+        // Os parâmetros nomeados permitem que você passe argumentos
+        // para um método especificando o nome do parâmetro, em vez de
+        // depender da ordem posicional.
+        // Isso melhora a legibilidade do código, especialmente quando um método tem muitos parâmetros.
+        // Ao chamar um método, você especifica o nome do parâmetro seguido por dois pontos (:) e o valor do argumento.
         public static void Formatar(int dia, int mes, int ano) {
             Console.WriteLine("{0:D2}/{1:D2}/{2}", dia, mes, ano);
         }
--- ClassesEMetodos/Params.cs
+++ ClassesEMetodos/Params.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace CursoCSharp.ClassesEMetodos {
     class Params {
-
+        // O parâmetro params permite que você passe um número variável de argumentos para um método.
+        // Ele é útil quando você não sabe quantos argumentos serão passados antecipadamente.
+        // O parâmetro params deve ser o último parâmetro na lista de parâmetros do método.
+        // Ele é declarado usando a palavra-chave params seguida por um tipo de array.
+        // Você pode passar um array do tipo especificado ou uma lista de argumentos separados por vírgulas.
+        // O compilador C# transforma a lista de argumentos em um array do tipo especificado.
+        // Exemplo:
         public static void Recepcionar(params string[] pessoas) {
             foreach (var pessoa in pessoas) {
                 Console.WriteLine($"Olá {pessoa}");
