6bd5deb6a4f67b429480e99be6cd4583b150afda
Raphael Lins <rapharell06@gmail.com>
2025-04-24 00:05:56 -0300

    Adicionando comentários explicativos sobre o uso de delegates na classe Delegate para melhor compreensão do código.

--- MetodosEFuncoes/Delegate.cs
+++ MetodosEFuncoes/Delegate.cs
@@ -5,6 +5,16 @@ using System.Threading.Tasks;
 
 namespace CursoCSharp.MetodosEFuncoes
 {
+    // Delegate é um tipo que referencia um método.
+    // É como se fosse um ponteiro para um método, mas com segurança de tipo.
+    // Ele pode ser usado para passar métodos como parâmetros, ou para armazenar métodos em variáveis.
+    // O delegate é definido com a palavra-chave "delegate", seguida do tipo de retorno e dos parâmetros do método.
+    // O delegate pode ser usado para criar métodos anônimos, que são métodos sem nome.
+    // O delegate pode ser usado para criar lambdas, que são expressões que representam um método anônimo.
+    // O delegate pode ser usado para criar eventos, que são uma forma de notificar outras classes quando algo acontece.
+    // O delegate pode ser usado para criar callbacks, que são métodos que são chamados quando uma operação assíncrona é concluída.
+    // O delegate pode ser usado para criar funções de ordem superior, que são funções que recebem outras funções como 
+    // parâmetros ou retornam funções como resultado.
     delegate double Operacao(double x, double y);
     public class LambdasDelegate
     {
