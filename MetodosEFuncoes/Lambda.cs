996fda5f41e92a0f93a3f395e739b45564bc8e3c
Raphael Lins <rapharell06@gmail.com>
2025-04-23 23:43:14 -0300

    Atualizando a classe Lambda com comentários explicativos sobre expressões lambda e delegates

--- MetodosEFuncoes/Lambda.cs
+++ MetodosEFuncoes/Lambda.cs
@@ -7,9 +7,27 @@ namespace CursoCSharp.MetodosEFuncoes
 {
     public class Lambda
     {
+        // Lambda é uma expressão que pode ser usada para criar funções anônimas
+        // ou expressões lambda. Elas são frequentemente usadas em LINQ e em outras operações de manipulação de dados.
+        // Uma expressão lambda é composta por uma lista de parâmetros, o operador "=>" e uma expressão ou bloco de código.
+        // A sintaxe básica de uma expressão lambda é a seguinte:
+        // (parametros) => { bloco de código }
+        // ou
+        // (parametros) => expressao
         public static void Executar(){
 
-            
+            // Action é um delegate que não retorna valor
+            Action outputNoConsole = () => {
+                Console.WriteLine("Olá Mundo com Lambda! Esse é sem parâmetro.");
+            };
+            Console.WriteLine("-------------------------------------------------");
+            outputNoConsole();
+            Console.WriteLine("-------------------------------------------------");
+
+            // Func é um delegate que retorna valor
+            Func<int> numeroDaSorte = () => new Random().Next(1, 1000);
+            Console.WriteLine("Função com Lambda: Esse é com parâmetro.");
+            Console.WriteLine($"Número da sorte: {numeroDaSorte()}");
 
             Console.WriteLine("-------------------------------------------------");
             Console.WriteLine("Pressione Enter para continuar...");
